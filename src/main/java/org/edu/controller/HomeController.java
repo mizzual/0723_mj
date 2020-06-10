e773b2758153cf5eb9f188527066f9c2d9693510
boramcom <boramcom@daum.net>
2020-06-10 14:12:51 +0900

    포트폴리오 경로 변경

--- src/main/java/org/edu/controller/HomeController.java
+++ src/main/java/org/edu/controller/HomeController.java
@@ -22,55 +22,64 @@ public class HomeController {
 	/**
 	 * 슬라이드 페이지 파일 입니다.
 	 */
-	@RequestMapping(value = "/slide", method = RequestMethod.GET)
+	@RequestMapping(value = "/sample/slide", method = RequestMethod.GET)
 	public String slide(Locale locale, Model model) {
 		
-		return "slide";
+		return "sample/slide";
 	}
 	
 	/**
 	 * CONTACT US 페이지 파일 입니다.
 	 */
-	@RequestMapping(value = "/contactus", method = RequestMethod.GET)
+	@RequestMapping(value = "/sample/contactus", method = RequestMethod.GET)
 	public String contactus(Locale locale, Model model) {
 		
-		return "contactus";
+		return "sample/contactus";
 	}
 	
 	/**
 	 * BLOG 페이지 파일 입니다.
 	 */
-	@RequestMapping(value = "/blog", method = RequestMethod.GET)
+	@RequestMapping(value = "/sample/blog", method = RequestMethod.GET)
 	public String blog(Locale locale, Model model) {
 		
-		return "blog";
+		return "sample/blog";
 	}
 	
 	/**
 	 * WORK 페이지 파일 입니다.
 	 */
-	@RequestMapping(value = "/work", method = RequestMethod.GET)
+	@RequestMapping(value = "/sample/work", method = RequestMethod.GET)
 	public String work(Locale locale, Model model) {
 		
-		return "work";
+		return "sample/work";
 	}
 	
 	/**
 	 * we are 페이지 파일 입니다.
 	 */
-	@RequestMapping(value = "/weare", method = RequestMethod.GET)
+	@RequestMapping(value = "/sample/weare", method = RequestMethod.GET)
 	public String weare(Locale locale, Model model) {
 		
-		return "weare";
+		return "sample/weare";
 	}
 	
 	/**
 	 * html5 테스트용 파일 입니다.
 	 */
-	@RequestMapping(value = "/htmltest", method = RequestMethod.GET)
+	@RequestMapping(value = "/sample/htmltest", method = RequestMethod.GET)
 	public String htmltest(Locale locale, Model model) {
 		
-		return "htmltest";
+		return "sample/htmltest";
+	}
+	
+	/**
+	 * 샘플 파일 홈 입니다.
+	 */
+	@RequestMapping(value = "/sample/", method = RequestMethod.GET)
+	public String sample(Locale locale, Model model) {
+		
+		return "sample/home";
 	}
 	
 	/**
