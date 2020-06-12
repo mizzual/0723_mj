8e4e3ec43a8f1201ccafa8ff58c344b92d558730
boramcom <boramcom@daum.net>
2020-06-12 14:40:51 +0900

    관리자단 AdminLTE 게시판 리스트 디자인 작업공간 board_list.html

--- src/test/java/kr/or/test/RegEx.java
+++ src/test/java/kr/or/test/RegEx.java
@@ -5,13 +5,13 @@ import java.util.regex.Pattern;
 public class RegEx {
 
 	public static void main(String[] args) {
-		String id = "5Angel1004";
+		String id = "A123456789";
 		String regExp = "[a-zA-Z][a-zA-Z0-9]{7,11}";
 		boolean isMatch = Pattern.matches(regExp, id);
 		if(isMatch) {
 			System.out.println("아이디로 사용가능 합니다.");
 		}else {
-			System.out.println("아이디로 영문 소문자, 숫자와 특수기호(_),(-)만 사용  가능합니다.");
+			System.out.println("아이디는 최소 8자 최대 12자까지 가능 합니다. 영문 소문자 사용  가능합니다.");
 		}
 	}
 
