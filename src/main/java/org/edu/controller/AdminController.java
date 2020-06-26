24ebea52d6999220a2adae8a2498004bf9c80247
boramcom <boramcom@daum.net>
2020-06-26 11:09:19 +0900

    관리자단 회원리스트 출력OK

--- src/main/java/org/edu/controller/AdminController.java
+++ src/main/java/org/edu/controller/AdminController.java
@@ -1,7 +1,12 @@
 package org.edu.controller;
 
+import java.util.List;
 import java.util.Locale;
 
+import javax.inject.Inject;
+
+import org.edu.service.IF_MemberService;
+import org.edu.vo.MemberVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AdminController {
+	
+	@Inject
+	private IF_MemberService memberService;
 	
 	/**
 	 * 게시물관리 리스트 입니다.
@@ -21,10 +29,14 @@ public class AdminController {
 	
 	/**
 	 * 회원관리 리스트 입니다.
+	 * @throws Exception 
 	 */
 	@RequestMapping(value = "/admin/member/list", method = RequestMethod.GET)
-	public String memberList(Locale locale, Model model) {
-		
+	public String memberList(Locale locale, Model model) throws Exception {
+		List<MemberVO> list = memberService.selectMember();
+		//모델클래스로 jsp화면으로 memberService에서 셀렉트한 list값을 memberList변수명으로 보낸다.
+		//model { list -> memberList -> jsp }
+		model.addAttribute("memberList", list);
 		return "admin/member/list";
 	}
 	
--- src/main/webapp/WEB-INF/views/admin/include/footer.jsp
+++ src/main/webapp/WEB-INF/views/admin/include/footer.jsp
@@ -41,7 +41,8 @@ $(document).ready(function() {
         if(current=="/admin" || current=="/admin/") {
         	
         }else{
-	        if($this.attr('href').includes(current) == true){
+	        //if($this.attr('href').includes(current) == true){
+	        if($this.attr('href').indexOf(current) != -1){
 	            $this.addClass('active');
 	        }else{
 	        	$this.removeClass('active');
